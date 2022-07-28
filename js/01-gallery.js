877cbb30816bce0bbf2fd7f16547ed7479bc75ee
Evgenija-P <e.a.poduzova@gmail.com>
2022-07-28 23:00:03 +0300

    tests 2 + 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,8 +3,6 @@ import { galleryItems } from "./gallery-items.js";
 
 const divGallery = document.querySelector(`.gallery`);
 
-divGallery.addEventListener("click", openImage);
-
 //------- Создаем разметку -------
 const galleryImages = galleryItems
   .map(
@@ -22,55 +20,24 @@ alt="${item.description}"/>
 divGallery.insertAdjacentHTML("afterbegin", galleryImages);
 
 //------- Открытие карточки -------
-function openImage(event) {
+const openImage = (event) => {
   event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-  const imgaeOrigin = event.target.dataset.source;
-  // console.log(imgaeOrigin);
 
-  const instance = basicLightbox.create(modal(imgaeOrigin));
-  const button = instance.element().querySelector("button");
-  button.addEventListener("click", instance.close);
+  // -----------Модалка запуск-----------
+  const instance = basicLightbox.create(
+    `<div class="modal">
+        <img src = "${event.target.dataset.source}" />
+    </div>`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", (event) => {
+          if (event.key === `Escape`) {
+            instance.close();
+          }
+        });
+      },
+    }
+  );
   instance.show();
-  console.log(instance);
-}
-
-// -----------Модалка -----------
-const modal = (image) =>
-  `<div class="modal">
-    <div class="modal-body">
-      <p>${galleryItems.original}</p>
-      <button>close</button>
-    </div>
-  </div>`;
-
-// const image = document.querySelector(".image");
-// console.log(image.src); // https://placeimg.com/640/480/animals
-// image.src = "https://placeimg.com/640/480/tech";
-
-// const viewItem = (id) => {
-//   const currentItem = items.find((item) => item.id === id);
-//   const instance = basicLightbox.create(modalTemplate(currentItem.text));
-//   const button = instance.element().querySelector("button");
-
-//   button.addEventListener("click", instance.close);
-//   instance.show();
-// };
-// const modalTemplate = (text = "") =>
-//   `<div class="modal">
-//     <div class="modal-body">
-//       <p>${text}</p>
-//       <button>close</button>
-//     </div>
-//   </div>`;
-// function selectColor(event) {
-//     if (event.target.nodeName !== "BUTTON") {
-//       return;
-//     }
-
-//     const selectedColor = event.target.dataset.color;
-//     output.textContent = `Selected color: ${selectedColor}`;
-//     output.style.color = selectedColor;
-//   }
+};
+divGallery.addEventListener("click", openImage);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const divGallery = document.querySelector(`.gallery`);
+
+//------- Создаем разметку -------
+const galleryImages = galleryItems
+  .map(
+    (item) =>
+      `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" 
+  src="${item.preview}" 
+  alt="${item.description}" />
+</a>`
+  )
+  .join("");
+
+divGallery.insertAdjacentHTML("afterbegin", galleryImages);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
